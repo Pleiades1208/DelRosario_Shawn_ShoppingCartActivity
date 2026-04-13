7201413c325644e4b5834c308553e0a1e30ed9d6
Shawn Cedric Del Rosario <shawndelrosario05@gmail.com>
2026-04-13 22:29:36 +0800

    Initial Project Setup: Product Class and Store Menu Display

--- ShoppingCart/Program.cs
+++ ShoppingCart/Program.cs
@@ -1,1 +1,50 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ShoppingCartSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product[] products = new Product[]
+            {
+                new Product(101, "Eggs", 230, 200),
+                new Product(102, "Bread", 75, 100),
+                new Product(103, "Milk", 150, 100)
+            };
+
+            Console.WriteLine("ID     NAME        PRICE       STOCK");
+
+            for (int i = 0; i < products.Length; i++)
+            {
+                products[i].DisplayProduct();
+            }
+        }
+    }
+
+    class Product
+    {
+        public int Id;
+        public string Name;
+        public double Price;
+        public int RemainingStock;
+
+        public Product(int id, string name, double price, int stock)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            RemainingStock = stock;
+        }
+
+        public void DisplayProduct()
+        {
+            Console.WriteLine($"{Id,3}   {Name,-10}   {Price,8:N2}   {RemainingStock,5}");
+        }
+
+        public double GetItemTotal(int quantity)
+        {
+            return Price * quantity;
+        }
+    }
+}
