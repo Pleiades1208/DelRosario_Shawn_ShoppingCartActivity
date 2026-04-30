156fc8afa6030a77bc6322df937a3be24c011dbd
Shawn Cedric Del Rosario <shawndelrosario05@gmail.com>
2026-04-30 17:13:12 +0800

    Addressed the issue of "no file per class organization", creating a new class Product.cs

---
+++ ShoppingCart/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCartSystem
+{
+    class Product
+    {
+        public int Id;
+        public string Name;
+        public double Price;
+        public int RemainingStock;
+
+        public Product(int id, string name, double price, int stock)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            RemainingStock = stock;
+        }
+
+        public void DisplayProduct()
+        {
+            Console.WriteLine($"{Id,3}   {Name,-10}   {Price,8:N2}   {RemainingStock,5}");
+        }
+
+        public double GetItemTotal(int quantity)
+        {
+            return Price * quantity;
+        }
+    }
+}
--- ShoppingCart/Program.cs
+++ ShoppingCart/Program.cs
@@ -189,29 +189,5 @@ namespace ShoppingCartSystem
         }
     }
 
-    class Product
-    {
-        public int Id;
-        public string Name;
-        public double Price;
-        public int RemainingStock;
-
-        public Product(int id, string name, double price, int stock)
-        {
-            Id = id;
-            Name = name;
-            Price = price;
-            RemainingStock = stock;
-        }
-
-        public void DisplayProduct()
-        {
-            Console.WriteLine($"{Id,3}   {Name,-10}   {Price,8:N2}   {RemainingStock,5}");
-        }
-
-        public double GetItemTotal(int quantity)
-        {
-            return Price * quantity;
-        }
-    }
+    
 }
